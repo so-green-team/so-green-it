855d3593eb6358ad3857ef09a7be986339f96af4
rigma <rigbuntu@gmail.com>
2017-11-03 15:42:04 +0100

    Adding an help message for /scan namespace

--- sogreenit/scan.py
+++ sogreenit/scan.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
 
+from flask import request, jsonify
 from sogreenit import app
 
-@app.route('/scan/static')
+@app.route('/scan', methods=['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'])
+def scan_help():
+    """
+    Return an little help about this namespace
+    """
+    return jsonify(
+        help={
+            '/scan/static': {
+                'method': 'POST',
+                'body': 'Your scanning parameters in JSON (go see ...)',
+                'description': 'Launch a eco-scan of one webpage only specified by the input URL'
+            },
+            '/scan/app': {
+                'method': 'POST',
+                'body': 'Your scanning parameters in JSON (go see ...)',
+                'description': 'Launch an eco-scan of a web application specified by the input URL'
+            }
+        }
+    )
+
+@app.route('/scan/static', methods=['POST'])
 def scan_static():
     """
     Launch a static scan of one web page
     """
-    return 'static'
+    pass
 
-@app.route('/scan/app')
+@app.route('/scan/app', methods=['POST'])
 def scan_app():
     """
     Launch a global scan of a web page
     """
-    return 'app'
+    pass
