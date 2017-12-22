4bbf1f7338f54ec157f7d4fbd0c3710c845e374e
Benjamin-eliot Bouguerra <benjamin-eliot.bouguerra@efrei.net>
2017-12-22 11:01:13 +0100

    removed abc

--- sogreenit/tests/test.py
+++ sogreenit/tests/test.py
@@ -1,14 +1,12 @@
 #TODO abstract class that raise an exception when the run method is called
-from abc import ABC
 
 class TestException(Exception):
     """Exception raised when a wrong call to run is done"""
     pass
 
-class Test(ABC):
+class Test():
     """Base class for all rules tested"""
 
     @staticmethod
-    @abstractmethod
     def run():
         raise TestException
