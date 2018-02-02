f8a17503245b4718cc0fd9102f8b23222b88c6f1
rigma <rigbuntu@gmail.com>
2018-02-02 09:44:45 +0100

    Fixing browsermob proxy path obn GNU/Linux

--- sogreenit/scan.py
+++ sogreenit/scan.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-import json
 import os
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
 
@@ -25,39 +21,43 @@ browsermob_server = None
 browsermob_proxy = None
 
 if os.name == 'nt':
-    browsermob_server = Server('{}\\browsermob-proxy-2.1.4\\bin\\browsermob-proxy.bat'.format(os.getcwd()))
+    browsermob_server = Server(
+        '{}\\browsermob-proxy-2.1.4\\bin\\browsermob-proxy.bat'.format(os.getcwd())
+    )
 else:
-    browsermob_server = Server('{}\\browsermob-proxy-2.1.4\\bin\\browsermob-proxy'.format(os.getcwd()))
+    browsermob_server = Server(
+        '{}/browsermob-proxy-2.1.4/bin/browsermob-proxy'.format(os.getcwd())
+    )
 
 browsermob_server.start()
 browsermob_proxy = browsermob_server.create_proxy()
 
 # Setting browser profile
-profile = webdriver.FirefoxProfile()
-profile.set_preference('app.update.enabled', False)
-profile.set_preference('browser.cache.disk.enable', False)
-profile.set_preference('browser.cache.disk.free_space_hard_limit', False)
-profile.set_preference('browser.cache.disk.free_space_soft_limit', False)
-profile.set_preference('browser.cache.disk.max_chunks_memory_usage', False)
-profile.set_preference('browser.cache.disk.max_entry_size', False)
-profile.set_preference('browser.cache.disk.max_priority_chunks_memory_usage', False)
-profile.set_preference('browser.cache.disk.metadata_memory_limit', False)
-profile.set_preference('browser.cache.disk.preload_chunk_count', False)
-profile.set_preference('browser.cache.disk.smart_size.enabled', False)
-profile.set_preference('browser.cache.disk.smart_size.first_run', False)
-profile.set_preference('browser.cache.disk.smart_size_cached_value', False)
-profile.set_preference('browser.cache.disk.smart_size.use_old_max', False)
-profile.set_preference('browser.cache.disk_cache_ssl', False)
-profile.set_preference('browser.cache.memory.enable', False)
-profile.set_preference('browser.cache.memory.max_entry_size', False)
-profile.set_preference('browser.cache.offline.enable', False)
-profile.set_preference('browser.cache.use_new_backend', False)
-profile.set_preference('browser.cache.use_new_backend_temp', False)
-profile.set_preference('dom.caches.enabled', False)
-profile.set_preference('dom.requestcache.enabled', False)
-profile.set_preference('image.cache.size', False)
-profile.set_preference('media.cache_size', False)
-profile.set_proxy(browsermob_proxy.selenium_proxy())
+browser_profile = webdriver.FirefoxProfile()
+browser_profile.set_preference('app.update.enabled', False)
+browser_profile.set_preference('browser.cache.disk.enable', False)
+browser_profile.set_preference('browser.cache.disk.free_space_hard_limit', False)
+browser_profile.set_preference('browser.cache.disk.free_space_soft_limit', False)
+browser_profile.set_preference('browser.cache.disk.max_chunks_memory_usage', False)
+browser_profile.set_preference('browser.cache.disk.max_entry_size', False)
+browser_profile.set_preference('browser.cache.disk.max_priority_chunks_memory_usage', False)
+browser_profile.set_preference('browser.cache.disk.metadata_memory_limit', False)
+browser_profile.set_preference('browser.cache.disk.preload_chunk_count', False)
+browser_profile.set_preference('browser.cache.disk.smart_size.enabled', False)
+browser_profile.set_preference('browser.cache.disk.smart_size.first_run', False)
+browser_profile.set_preference('browser.cache.disk.smart_size_cached_value', False)
+browser_profile.set_preference('browser.cache.disk.smart_size.use_old_max', False)
+browser_profile.set_preference('browser.cache.disk_cache_ssl', False)
+browser_profile.set_preference('browser.cache.memory.enable', False)
+browser_profile.set_preference('browser.cache.memory.max_entry_size', False)
+browser_profile.set_preference('browser.cache.offline.enable', False)
+browser_profile.set_preference('browser.cache.use_new_backend', False)
+browser_profile.set_preference('browser.cache.use_new_backend_temp', False)
+browser_profile.set_preference('dom.caches.enabled', False)
+browser_profile.set_preference('dom.requestcache.enabled', False)
+browser_profile.set_preference('image.cache.size', False)
+browser_profile.set_preference('media.cache_size', False)
+browser_profile.set_proxy(browsermob_proxy.selenium_proxy())
 
 # DB connection
 # db = DBConnection(host=os.getenv('SOGREEN_DB_HOST'))
@@ -100,11 +100,12 @@ def scan_static():
     driver = webdriver.Remote(
         command_executor='http://127.0.0.1:4444/wd/hub',
         desired_capabilities={
-            'applicationCacheEnabled': False, # Subject to change: modern web application trends to use those functionalities
+            # Subject to change: modern web application trends to use those functionalities
+            'applicationCacheEnabled': False,
             'webStorageEnabled': False,
             'databaseEnabled': False
         },
-        browser_profile=profile
+        browser_profile=browser_profile
     )
 
     # Loading input URL
@@ -152,7 +153,7 @@ def scan_static():
 
     # Computing tests
     for rule_id in rules_set:
-        if rule_id not in user_params['excludeRules']:
+        if rule_id not in excluded_rules:
             results[rule_id] = tests[rule_id].run(har, dom, cpu, mem)
 
     # Closing browser
