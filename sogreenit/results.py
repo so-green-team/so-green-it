04f5526af2b9c0e513d63977282a0e979f817dbd
rigma <rigbuntu@gmail.com>
2018-02-13 20:34:55 +0100

    Adding routes mapping for results

--- sogreenit/results.py
+++ sogreenit/results.py
@@ -1,1 +1,9 @@
-# TODO: managing of results are still in discussion
+from sogreenit import app, db
+
+@app.route('/results/<project_id:int>')
+def retrieve_project_results(project_id):
+    pass
+
+@app.route('/results/<page_id:int>')
+def retrieve_page_results(page_id):
+    pass
