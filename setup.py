42506f18e9b912383d9f1a53462b40b47f79cef6
rigma <rigbuntu@gmail.com>
2018-02-14 22:40:44 +0100

    Changing package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="so-green-it",
-    version="0.1",
+    version="0.5",
     packages=['sogreenit'],
     include_package_data=True,
     install_requires=[
--- sogreenit/__init__.py
+++ sogreenit/__init__.py
@@ -7,7 +7,7 @@ import json
 import os
 
 VERSION_MAJOR = 0
-VERSION_MINOR = 1
+VERSION_MINOR = 5
 app = Flask(__name__)
 
 input_schema = {
